ae9b98622f1fd636e5b27c23c6c2c658dd6cae3b
Harshit Garg <ggharshit1@gmail.com>
2023-06-17 22:35:17 +0530

    PeopleBar added

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,15 +1,23 @@
+import getUsers from "../actions/getUsers"
 import Sidebar from "../components/sidebar/Sidebar"
+import UserList from "./components/UserList"
 
 export default async function UsersLayout({
     children
 }: {
     children: React.ReactNode
 }) {
-    return(
+
+    const users = await getUsers()
+
+    return (
         <Sidebar>
             <div
                 className="h-full "
             >
+                <UserList
+                    items={users!}
+                />
                 {children}
             </div>
         </Sidebar>
